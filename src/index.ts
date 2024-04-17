5d1e81cadb209fdd7d03f8a37d1836816b73adf2
Umut Topalak <umuttopalak@hotmail.com>
2024-04-17 22:24:54 +0300

    logs removed

--- src/index.ts
+++ src/index.ts
@@ -11,40 +11,31 @@ async function run() {
     const virtual_env: string = core.getInput("virtual_env");
 
     const console_url: string = `https://${host}/api/v0/user/${username}/consoles/${console_id}/send_input/`;
-    let payload  = {}
-    let response = {}
+    let payload = {};
+    let response = {};
 
-    console.log("Starting Virtual Environment");
     payload = {
       input: `source /${virtual_env}/bin/activate`,
     };
     response = await axios.post(console_url, payload, {
       headers: { Authorization: `Token ${api_token}` },
     });
-    console.log("Success.");
 
-    console.log("Running `git pull`.");
     payload = { input: "git pull\n" };
     response = await axios.post(console_url, payload, {
       headers: { Authorization: `Token ${api_token}` },
     });
-    console.log("Success.");
 
-    console.log("Running `pip install -r requirements.txt`.");
     payload = { input: "pip install -r requirements.txt\n" };
     response = await axios.post(console_url, payload, {
       headers: { Authorization: `Token ${api_token}` },
     });
-    console.log("Success.");
 
-    console.log("Running `python manage.py migrate`.");
     payload = { input: "python manage.py migrate\n" };
     response = await axios.post(console_url, payload, {
       headers: { Authorization: `Token ${api_token}` },
     });
-    console.log("Success.");
 
-    console.log("Reloading webapp.");
     const url: string = `https://${host}/api/v0/user/${username}/webapps/${domain_name}/reload/`;
     response = await axios.post(
       url,
@@ -53,7 +44,6 @@ async function run() {
         headers: { Authorization: `Token ${api_token}` },
       }
     );
-    console.log("Reloaded webapp successfully.");
   } catch (error) {
     core.setFailed(
       error instanceof Error ? error.message : JSON.stringify(error)
